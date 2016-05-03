252da8acda25cc02bdfcb7f88c7eb0e660d1d2e9
srxlpx <edloran75@gmail.com>
2016-05-03 08:52:58 -0500

    Initial commit

---
+++ app/src/main/java/com/srxlpx/mimarciano3semana/MainActivity.java
@@ -0,0 +1,13 @@
+package com.srxlpx.mimarciano3semana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
